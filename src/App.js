cc7e0f1592bc4b8a017ce373f0a34088b98dff2d
UzomaAkpala <francisakpala@yahoo.com>
2023-08-30 12:32:08 +0100

    children props

--- src/App.js
+++ src/App.js
@@ -50,21 +50,13 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handlePrevious}
-              text="Previous"
-              emoji="⏮️"
-            />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              Previous <span>⏮️</span>
+            </Button>
 
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handleNext}
-              text="Next"
-              emoji=" ⏭️"
-            />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>⏭️</span>
+            </Button>
           </div>
         </div>
       )}
@@ -72,15 +64,14 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text, emoji }) {
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <>
       <button
         style={{ backgroundColor: bgColor, color: textColor }}
         onClick={onClick}
       >
-        <span>{emoji}</span>
-        {text}
+        {children}
       </button>
     </>
   );
